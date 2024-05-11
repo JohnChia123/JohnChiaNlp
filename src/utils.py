d899794485b2ef5f42de28f16e96d7dd8a2d3d5e
John Chia Heng Kit <chatriver1@gmail.com>
2024-05-12 00:11:55 +0800

    First Edition of Logistic Regression Complete

--- src/utils.py
+++ src/utils.py
@@ -1,2 +1,25 @@
+import re
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import stopwords
+
+import nltk
+nltk.download('stopwords')
+nltk.download('wordnet')
+
+# Preprocessing function
+def preprocess_text(text):
+    # Convert to lower case
+    text = text.lower()
+    # Remove non-alphanumeric characters
+    text = re.sub(r'[^a-zA-Z0-9\s]', '', text)
+    # Tokenize text
+    tokens = text.split()
+    # Remove stopwords
+    tokens = [word for word in tokens if word not in stopwords.words('english')]
+    # Lemmatize
+    lemmatizer = WordNetLemmatizer()
+    tokens = [lemmatizer.lemmatize(word) for word in tokens]
+    return ' '.join(tokens)
+
 def addOne(x):
     return x + 1
